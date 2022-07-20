413de5209c8fbff0a64bf203af897059c41cc21d
Neliane de Oliveira <92684306+Nelianeoliveira@users.noreply.github.com>
2022-07-20 19:04:40 -0300

    Initial commit

---
+++ src/br/com/exercicios/loops/Nota.java
@@ -0,0 +1,19 @@
+package br.com.exercicios.loops;
+
+import java.util.Scanner;
+
+public class Nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Notas: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 | nota > 10) {
+            System.out.println("Nota Inválida! Digite novamente: ");
+            nota = scan.nextInt();
+        }
+    }
+}
